072fc5f78b98e4762b7af7bd10b9965c79f7b99e
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-16 05:22:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return if (string === string.toUpperCase()) {
+  return if (string === string.toLowerCase()) {
     console.log('I can't hear you!)
   }
 }
