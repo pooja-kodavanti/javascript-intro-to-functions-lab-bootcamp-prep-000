46dd0bf6f5795b61beeec2e5d26974689bd170e3
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-16 05:41:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,15 +17,7 @@ function sayHiToGrandma(string) {
   else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  else if (string === "I love you, Grandma." "
-}
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
-}
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
+  else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
